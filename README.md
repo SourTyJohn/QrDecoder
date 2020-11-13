cac27de8504ae69b5dd5946beb2ee190f39f026e
JohnSour <mr.ukhov@list.ru>
2020-11-13 10:40:00 +0400

    v 1.1

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
     main.py: scanning all images in 'to_scan' folder 
     and prints data in terminal
+    
+
+-    
+
+    name of image can only contain utf-8 symbols
+    
--- main.py
+++ main.py
@@ -5,15 +5,23 @@ import cv2
 detector = cv2.QRCodeDetector()
 
 
-files = [join('to_scan', x) for x in listdir('to_scan')]
-
-for file in files:
-    try:
+def scan(f):
+    for file in f:
         img = cv2.imread(file)
+
+        if img is None:
+            print(f"{file} \t  !Error while reading (try to change file name)!\n")
+            continue
+
         data, bbox, straight_qrcode = detector.detectAndDecode(img)
 
         if bbox is not None:
-            print(f"{file} \t\t\t ~ {data}\n")
+            print(f"{file} \t   Data: {data}\n")
 
-    except Exception as error:
-        print(error, '\n')
+        else:
+            print(f"{file} \t  !No Data!\n")
+
+
+if __name__ == '__main__':
+    files = [join('to_scan', x) for x in listdir('to_scan')]
+    scan(files)
